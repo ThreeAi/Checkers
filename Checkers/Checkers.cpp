2f34fd2ce42d3757506b892c177dd1f866363fcb
Артём <alexandrovaa2003@gmail.com>
2022-11-21 20:12:34 +0300

    4

--- Checkers/Checkers.cpp
+++ Checkers/Checkers.cpp
@@ -5,7 +5,7 @@
 using namespace sf;
 int main()
 {
-	RenderWindow window(VideoMode(71 * 8 + 2 * 8, 71 * 8 + 2 * 8), "Checkers");
+	RenderWindow window(VideoMode(LENGTH_TILE * 8 + 2 * WIGTH_EDGE, LENGTH_TILE * 8 + 2 * WIGTH_EDGE), "Checkers");
 	Texture textboard, textchecker;
 	textboard.loadFromFile("images/board.png");
 	textchecker.loadFromFile("images/checker.png");
@@ -15,6 +15,9 @@ int main()
 	shapeChecker.setTexture(&textchecker);
 	shapeChecker.setTextureRect(IntRect(0, 0, 112, 112));
 	shapeChecker.setPosition(9, 9);
+	RectangleShape background(Vector2f(LENGTH_TILE * 8 + 2 * WIGTH_EDGE, LENGTH_TILE * 8 + 2 * WIGTH_EDGE));
+	background.setFillColor(Color(189, 183, 107));
+	background.setPosition(0, 0);
 	bool isMove = false;
 	float dX = 0, dY = 0;
 
@@ -22,20 +25,24 @@ int main()
 	{
 		Vector2i pixelPos = Mouse::getPosition(window);
 		Event event;
-		std::cout << pixelPos.x << "\n";
+		//std::cout << pixelPos.x << "\n";
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
 			if (event.type == Event::MouseButtonPressed)//если нажата клавиша мыши
 				if (event.key.code == Mouse::Left)//а именно левая
-					if (shapeChecker.getGlobalBounds().contains(pixelPos.x, pixelPos.y))//и при этом координата курсора попадает в спрайт
+				{
+					float y = shapeChecker.getGlobalBounds().top + 35;
+					float x = shapeChecker.getGlobalBounds().left + 35;
+					if (sqrt((pixelPos.y-y)* (pixelPos.y - y) + (pixelPos.x - x)* (pixelPos.x - x)) <= 35)//и при этом координата курсора попадает в спрайт
 					{
 						std::cout << "isClicked!\n";//выводим в консоль сообщение об этом
 						dX = pixelPos.x - shapeChecker.getPosition().x;//делаем разность между позицией курсора и спрайта.для корректировки нажатия
 						dY = pixelPos.y - shapeChecker.getPosition().y;//тоже самое по игреку
 						isMove = true;//можем двигать спрайт							
 					}
+				}
 			if (event.type == Event::MouseButtonReleased)//если отпустили клавишу
 				if (event.key.code == Mouse::Left) //а именно левую
 					isMove = false; //то не можем двигать спрайт
@@ -45,33 +52,19 @@ int main()
 			}
 		}
 		window.clear();
+		window.draw(background);
 		for (int i = 0; i < HEIGHT_BOARD; i++)
 			for (int j = 0; j < WIGTH_BOARD; j++)
 			{
-				if (TileBoard[i][j] == '4')
-				{
-					spriteboard.setTextureRect(IntRect(142, 0, 8, 8));
-					spriteboard.setPosition((i / 9) * (LENGTH_TILE * 8 + WIGTH_EDGE), (j / 9) * (LENGTH_TILE * 8 + WIGTH_EDGE));
-				}
-				if (TileBoard[i][j] == '2')
-				{
-					spriteboard.setTextureRect(IntRect(142, 0, 8, 71));
-					spriteboard.setPosition((i / 9) * (LENGTH_TILE * 8 + WIGTH_EDGE), LENGTH_TILE * (j - 1) + WIGTH_EDGE);
-				}
-				if (TileBoard[i][j] == '3')
-				{
-					spriteboard.setTextureRect(IntRect(142, 0, 71, 8));
-					spriteboard.setPosition(LENGTH_TILE * (i - 1) + WIGTH_EDGE, (j / 9) * (LENGTH_TILE * 8 + WIGTH_EDGE));
-				}
 				if (TileBoard[i][j] == '1')
 				{
 					spriteboard.setTextureRect(IntRect(0, 0, 71, 71));
-					spriteboard.setPosition(LENGTH_TILE * (i - 1) + WIGTH_EDGE, LENGTH_TILE * (j - 1) + WIGTH_EDGE);
+					spriteboard.setPosition(LENGTH_TILE * i + WIGTH_EDGE, LENGTH_TILE * j + WIGTH_EDGE);
 				}
 				if (TileBoard[i][j] == '0')
 				{
 					spriteboard.setTextureRect(IntRect(72, 0, 71, 71));
-					spriteboard.setPosition(LENGTH_TILE * (i - 1) + WIGTH_EDGE, LENGTH_TILE * (j - 1) + WIGTH_EDGE);
+					spriteboard.setPosition(LENGTH_TILE * i + WIGTH_EDGE, LENGTH_TILE * j + WIGTH_EDGE);
 				}
 				window.draw(spriteboard);
 			}
--- Checkers/Interface.h
+++ Checkers/Interface.h
@@ -2,18 +2,16 @@
 using namespace sf;
 const int WIGTH_EDGE = 8;
 const int LENGTH_TILE = 71;
-const int HEIGHT_BOARD = 10;
-const int WIGTH_BOARD = 10;
+const int HEIGHT_BOARD = 8;
+const int WIGTH_BOARD = 8;
 
-String TileBoard[WIGTH_BOARD] = {
-	"4222222224",
-	"3010101013",
-	"3101010103",
-	"3010101013",
-	"3101010103",
-	"3010101013",
-	"3101010103",
-	"3010101013",
-	"3101010103",
-	"4222222224",
+String TileBoard[LENGTH_TILE] = {
+	"01010101",
+	"10101010",
+	"01010101",
+	"10101010",
+	"01010101",
+	"10101010",
+	"01010101",
+	"10101010",
 };
